800fe4232639799e5dd9b17b5ca1226fbd8e5b3b
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-07-29 13:44:20 +0500

    Update python-google-maps.py

--- python-google-maps.py
+++ python-google-maps.py
@@ -1,6 +1,14 @@
 from pygeocoder import Geocoder
 
+import time
+
 
 placeName = input("Enter a place name: ")
 
+time.sleep(0.5)
+
 print("Searching Google Maps for {}".format(placeName))
+
+time.sleep(1)
+
+results = Geocoder.geocode(placeName)
